9461666e67303e08b69de17a4ee1624492b01456
vinoop vennekode <vinoopvennekode@gmail.com>
2023-02-11 16:32:29 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="bg-black text-white flex justify-center">
+      <h1>hiii</h1>
+    </div>
+  );
+}
+
+export default App;
