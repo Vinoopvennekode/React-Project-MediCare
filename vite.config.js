224b6904667749fbb962d8376f67cf5f7e7458ca
vinoop vennekode <vinoopvennekode@gmail.com>
2023-03-22 16:35:59 +0530

    build files to a directory named build,set the build.outDir property to 'build'

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    outDir: 'build' // set the output directory to 'build'
+  },
   plugins: [react()],
 })
+
