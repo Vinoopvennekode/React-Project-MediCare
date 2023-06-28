019708d13492bca2a519b06293e209872a93e68d
vinoop vennekode <vinoopvennekode@gmail.com>
2023-06-28 16:30:53 +0530

    change done

--- src/constants/copnstant.js
+++ src/constants/copnstant.js
@@ -1,9 +1,6 @@
 // const baseUrl ='https://back.medicarebooking.online/';
 const baseUrl ='https://backend-medicare.onrender.com/';
-<<<<<<< HEAD
 // const baseUrl='http://localhost:5000/'   
-=======
-// const baseUrl='http://localhost:5000/'
->>>>>>> bc7e586c02594e1934e9bca770a319e171c1627d
+
 
 export default baseUrl;
